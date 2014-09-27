fb96e7a41a553b703414293c6f24b4f93a1aae3a
edom18 <edo.m18@gmail.com>
2014-09-28 00:57:55 +0900

    updated build.

--- build/mathjs.js
+++ build/mathjs.js
@@ -414,7 +414,7 @@ function ajax(url, opt) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             if (xhr.status === 200 || xhr.status === 201) {
-                def.resolve(JSON.parse(xhr.responseText), xhr);
+                def.resolve(xhr.responseText, xhr);
                 def = null;
             }
             else {
@@ -556,7 +556,7 @@ function when(arr) {
 
     while(i--) {
         (function (index) {
-            arr[i].done(function (res) {
+            arr[index].done(function (res) {
                 _watch(res, index);
             });
         }(i));
@@ -3028,6 +3028,149 @@ ns.util = util;
     exports.vec4 = vec4;
 
 }(window, document, window));
+
+(function () {
+    'use strict';
+
+    /**
+     * Create a sphere.
+     *
+     * 極地から順に徐々に赤道へ角度を増やしながら、
+     * 球体の円周上に頂点を生成していく。
+     *
+     * @param {number} row
+     * @param {number} column
+     * @param {number} radius
+     * @param {Array<number>} color
+     *
+     * @return {Object}
+     */
+    function Sphere(row, column, radius, color) {
+        var position = [];
+        var normal   = [];
+        var colorArr = [];
+        var st       = [];
+        var index    = [];
+
+        // Generate position, normal, color, texture coord.
+        for (var row_i = 0; row_i <= row; row_i++) {
+            var r  = Math.PI / row * row_i;
+            var ry = Math.cos(r);
+            var rr = Math.sin(r);
+
+            for (var col_i = 0; col_i <= column; col_i++) {
+                var tr = Math.PI * 2 / column * col_i;
+
+                var tx = rr * radius * Math.cos(tr);
+                var ty = ry * radius;
+                var tz = rr * radius * Math.sin(tr);
+
+                var rx = rr * Math.cos(tr);
+                var rz = rr * Math.sin(tr);
+
+                var tc = color ? color : [1, 1, 1, 1];
+
+                position.push(tx, ty, tz);
+                normal.push(rx, ry, rz);
+                colorArr.push(tc[0], tc[1], tc[2], tc[3]);
+                st.push(1 - 1 / column * col_i, 1 / row * row_i);
+            }
+        }
+
+        // Generate indecies.
+        for (r = 0, row_i = 0; row_i < row; row_i++) {
+            for (col_i = 0; col_i < column; col_i++) {
+                r = (column + 1) * row_i + col_i;
+                var idx0 = r;
+                var idx1 = r + 1;
+                var idx2 = r + column + 2;
+                var idx3 = r + column + 1;
+                index.push(idx0, idx1, idx2);
+                index.push(idx0, idx2, idx3);
+            }
+        }
+
+        this.position = position;
+        this.normal   = normal;
+        this.color    = colorArr;
+        this.texture  = st;
+        this.index    = index;
+    }
+
+    /*! ----------------------------------------------------
+        EXPORTS.
+    -------------------------------------------------------- */
+    window.Sphere = Sphere;
+
+}());
+
+(function () {
+    'use strict';
+
+    function Torus(row, column, irad, orad, color) {
+        var position = [];
+        var normal   = [];
+        var colorArr = [];
+        var st       = [];
+        var index    = [];
+
+        for (var row_i = 0; row_i <= row; row_i++) {
+            var r  = Math.PI * 2 / row * row_i;
+            var rr = Math.cos(r);
+            var ry = Math.sin(r);
+
+            for (var col_i = 0; col_i <= column; col_i++) {
+                var tr = Math.PI * 2 / column * col_i;
+                var tx = (rr * irad * orad) * Math.cos(tr);
+                var ty = ry * irad;
+                var tz = (rr * irad * orad) * Math.sin(tr);
+
+                var rx = rr * Math.cos(tr);
+                var rz = rr * Math.sin(tr);
+
+                var tc = color ? color : [1, 1, 1, 1];
+
+                var rs = 1 / column * col_i;
+                var rt = 1 / row * row_i + 0.5;
+
+                if (rt > 1.0) {
+                    rt -= 1.0;
+                }
+
+                rt = 1.0 - rt;
+
+                position.push(tx, ty, tz);
+                normal.push(rx, ry, rz);
+                colorArr.push(tc[0], tc[1], tc[2], tc[3]);
+                st.push(rs, rt);
+            }
+        }
+
+        for (row_i = 0; row_i < row; row_i++) {
+            for (col_i = 0; col_i < column; col_i++) {
+                r = (column + 1) * row_i + col_i;
+                var idx0 = r;
+                var idx1 = r + column + 1;
+                var idx2 = r + 1;
+                var idx3 = r + column + 2;
+                index.push(idx0, idx1, idx2);
+                index.push(idx1, idx3, idx2);
+            }
+        }
+
+        this.position = position;
+        this.normal   = normal;
+        this.color    = colorArr;
+        this.texture  = st;
+        this.index    = index;
+    }
+
+    /*! --------------------------------------------------------
+        EXPORTS.
+    ------------------------------------------------------------ */
+    window.Torus = Torus;
+
+}());
 
 (function (win, doc, exports) {
 
